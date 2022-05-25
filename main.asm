773426b88881185938f0677d68dce5363deae91e
jsd1982 <james.jdunne@gmail.com>
2022-05-25 00:00:08 -0500

    hud: much better version of HUD hflip moved outside of NMI

--- main.asm
+++ main.asm
@@ -1,10 +1,15 @@
 lorom
 
-check title "ZELDANODENSETSU      "
+; patch against JP 1.0 ROMs and derivates
+;check title "ZELDANODENSETSU      "
 
 VMADDR = $002116
 VMADDL = $002116
 VMADDH = $002117
+
+VMDATA = $002118
+VMDATAL = $002118
+VMDATAH = $002119
 
 DMAENABLE = $00420B
 
@@ -14,13 +19,12 @@ DMA0ADDRH = $004303
 DMA0ADDRB = $004304
 DMA0SIZE = $004305
 
-; original routine in NMI:
-org $008B6B
+org $00877E
+;#_00877E: SEP #$20
+;#_008780: RTS
     JMP.w $CF46
-    NOP
-    NOP
-    NOP
 
+; original routine in NMI to update BG3 for HUD:
 org $008B71
 #_008B71: LDX.w #$7EC900>>0
 #_008B74: STX.w DMA0ADDRL
@@ -37,41 +41,61 @@ org $008B71
 ; free space for patch code:
 org $00CF46
 NULL_00CF46:
-    LDX.w $0219
-    STX.w VMADDR
+    SEP   #$20
+    LDA.b $16
+    BEQ .skip_BG3
 
-    REP   #$20
+    ; operate in bank $7E
+    PHB
+    LDA.b #$7E : PHA : PLB
+
+    REP   #$30
     LDA.w #$4000 ; hflip bit
     STA.b $0E
 
+    ; clear tilemap beyond $14A range up to $180
+    LDA.w #$207F
+    LDY.w #$7E-$4A
+-
+    STA.w $C700+$14A, Y
+    DEY : DEY
+    BPL -
+
     ; toggle hflip bit in each word and reverse X positions
     LDY.w #$0000
-    LDX.w #$0080
-
-flip:
-    LDA.l $7EC700+$000-2, X
+    LDX.w #$003E
+-
+    LDA.w $C700+$000, X
     EOR.w $0E
-    PHX : TYX
-    STA.l $7EC900+$000, X
-    PLX
+    STA.w $C900+$000, Y
 
-    LDA.l $7EC700+$080-2, X
+    LDA.w $C700+$040, X
     EOR.w $0E
-    PHX : TYX
-    STA.l $7EC900+$080, X
-    PLX
+    STA.w $C900+$040, Y
 
-    LDA.l $7EC700+$100-2, X
+    LDA.w $C700+$080, X
     EOR.w $0E
-    PHX : TYX
-    STA.l $7EC900+$100, X
-    PLX
+    STA.w $C900+$080, Y
+
+    LDA.w $C700+$0C0, X
+    EOR.w $0E
+    STA.w $C900+$0C0, Y
+
+    LDA.w $C700+$100, X
+    EOR.w $0E
+    STA.w $C900+$100, Y
+
+    LDA.w $C700+$140, X
+    EOR.w $0E
+    STA.w $C900+$140, Y
 
     INY : INY
     DEX : DEX
-    BNE   flip
+    BPL   -
 
-    SEP #$20
-    JMP.w $8B71
+    PLB
+.skip_BG3:
+    SEP   #$30
+    RTS
 
 warnpc $00CFC0
